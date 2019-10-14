3011fa7b6b995afd3b566079e7efeed6e352934a
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-14 16:36:45 -0500

    working on Routes, added background img and img src's to players

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,47 @@
 var friends = [
     {
         "name":"Paul McBeth",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.jomezpro.com/wp-content/uploads/2019/01/paul-mcbeth-819x1024.jpeg",
         "scores":[5,1,4,4,5,1,2,5,4,1]
     },
     {
         "name":"Ricky Wysocki",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://pbs.twimg.com/media/D446FOmWwAARGEs.jpg:large",
         "scores":[4,2,3,5,1,1,2,4,3,1]
     },
     {
         "name":"Paige Pierce",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.discgolfworldtour.com/wp-content/uploads/2017/10/DGWTPaigePierceProfile484.jpg",
         "scores":[1,1,2,3,3,4,5,1,3,5]
     },
     {
         "name":"Catrina Allen",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.pdga.com/files/styles/homepage_carousel/public/gbo-1.png?itok=6-dnMr5j",
         "scores":[2,3,5,5,4,4,5,2,1,1]
     },
     {
         "name":"Jeremy 'Big Jerm' Koling",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://cdn.discgolf.ultiworld.com/wp-content/uploads/2017/05/33249308553_f0dff4ec4e_k-1024x659.jpg",
         "scores":[5,1,5,1,3,4,5,1,2,3]
     },
     {
         "name":"Nate Sexton",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"//cdn.shopify.com/s/files/1/0981/0008/collections/Artboard_3_fb8e4cd4-9b13-4524-8f83-4eb3630a20e8_1024x1024.png?v=1562600217",
         "scores":[4,4,4,3,2,5,1,1,2,4]
     },
     {
         "name":"Paige Bjerkaas",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.pdga.com/files/styles/homepage_carousel/public/29602927207_e76d2efb61_k.jpg?itok=1NWD56oV",
         "scores":[5,4,2,1,3,2,3,1,4,5]
     },
     {
         "name":"Nikko Locastro",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.innovadiscs.com/wp-content/uploads/2019/10/nikko-locastro-usdgc-albatross-694x350.jpg",
         "scores":[5,1,4,4,3,5,1,1,5,4]
     },
     {
         "name":"Simon Lizotte",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.jomezpro.com/wp-content/uploads/2019/01/simon-lizotte.jpg",
         "scores":[5,1,4,4,5,1,4,4,2,3]
     },
 ];
