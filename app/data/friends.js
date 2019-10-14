5324dc4ab5b9e89045b4a93334440665f818c83d
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-13 20:10:04 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,47 @@
+var friends = [
+    {
+        "name":"Paul McBeth",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name":"Ricky Wysocki",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[4,2,3,5,1,1,2,4,3,1]
+    },
+    {
+        "name":"Paige Pierce",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[1,1,2,3,3,4,5,1,3,5]
+    },
+    {
+        "name":"Catrina Allen",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[2,3,5,5,4,4,5,2,1,1]
+    },
+    {
+        "name":"Jeremy 'Big Jerm' Koling",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5,1,5,1,3,4,5,1,2,3]
+    },
+    {
+        "name":"Nate Sexton",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[4,4,4,3,2,5,1,1,2,4]
+    },
+    {
+        "name":"Paige Bjerkaas",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5,4,2,1,3,2,3,1,4,5]
+    },
+    {
+        "name":"Nikko Locastro",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5,1,4,4,3,5,1,1,5,4]
+    },
+    {
+        "name":"Simon Lizotte",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5,1,4,4,5,1,4,4,2,3]
+    },
+];
